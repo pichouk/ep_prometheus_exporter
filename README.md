bf2185125f3596d569d030d8fff49dc18cf8cb77
Kyâne <kyane@kyane.fr>
2020-12-08 01:31:21 +0100

    First version

---
+++ README.md
@@ -0,0 +1,27 @@
+# ep_prometheus_exporter
+
+Setup a Prometheus exporter for Etherpad lite.
+
+You just have to install the plugin and configure your Prometheus server to scrape http://pad.mydomain.org/metrics. Here is an example result :
+
+```
+# HELP etherpad_pads_count Number of pads
+# TYPE etherpad_pads_count gauge
+etherpad_pads_count{instance="etherpad-test"} 6
+
+# HELP etherpad_blank_pads_count Number of blank pads
+# TYPE etherpad_blank_pads_count gauge
+etherpad_blank_pads_count{instance="etherpad-test"} 2
+
+# HELP etherpad_connected_users_count Number of connected users
+# TYPE etherpad_connected_users_count gauge
+etherpad_connected_users_count{instance="etherpad-test"} 1
+```
+
+You can set a specific `instance` label to your metrics with a configuration key on Etherpad `settings.json` file :
+
+```json
+"ep_prometheus_exporter": {
+  "instanceName": "etherpad-test"
+}
+```
---
+++ index.js
@@ -0,0 +1,108 @@
+const epAPI = require("ep_etherpad-lite/node/db/API");
+
+// Load settings
+const epSettings = require("ep_etherpad-lite/node/utils/Settings");
+const settings = epSettings.ep_prometheus_exporter
+  ? epSettings.ep_prometheus_exporter
+  : {};
+
+// Metrics
+var metrics = {
+  padsCount: 0,
+  blankPadsCount: 0,
+  connectedUsers: 0,
+};
+var blankPads = {};
+
+// Update all metrics
+async function updateMetrics() {
+  // Count all pads
+  const padsList = await epAPI.listAllPads();
+  metrics.padsCount = padsList.padIDs.length;
+
+  // Get number of blank pads
+  metrics.blankPadsCount = Object.keys(blankPads).length;
+
+  // Get Etherpad stats
+  const epStats = require("ep_etherpad-lite/node/stats").toJSON();
+
+  // Update connected users
+  metrics.connectedUsers = epStats.totalUsers;
+}
+
+// Update the blank pads list based on a pad event
+async function updateBlankPads(pad) {
+  if (pad.head === 0) {
+    blankPads[pad.id] = 1;
+  } else {
+    if (blankPads[pad.id] !== undefined) {
+      delete blankPads[pad.id];
+    }
+  }
+}
+
+// When a new pad is created
+exports.padCreate = async (hook_name, context) => {
+  // Update the blank pad list
+  await updateBlankPads(context.pad);
+  // Update all metrics
+  updateMetrics();
+};
+
+// When a pad is loaded (opened)
+exports.padLoad = async (hook_name, context) => {
+  // Update blank pad list
+  await updateBlankPads(context.pad);
+  metrics.blankPadsCount = Object.keys(blankPads).length;
+};
+
+// When a pad is updated
+exports.padUpdate = async (hook_name, context) => {
+  // Update blank pad list
+  await updateBlankPads(context.pad);
+  metrics.blankPadsCount = Object.keys(blankPads).length;
+};
+
+// When a pad is removed
+exports.padRemove = async (hook_name, context) => {
+  // Update blank pad list
+  var padId = context.padID;
+  if (blankPads[padId] !== undefined) {
+    delete blankPads[padId];
+  }
+  // Update metrics
+  updateMetrics();
+};
+
+// When Etherpad server is started
+exports.expressCreateServer = async (hook_name, args) => {
+  // Initialize metrics
+  await updateMetrics();
+  // Initialize blank pad count
+  const padsList = await epAPI.listAllPads();
+  padsList.padIDs.forEach(async (padID) => {
+    if ((await epAPI.getRevisionsCount(padID)).revisions <= 1) {
+      blankPads[padID] = 1;
+    }
+  });
+
+  // Get instance name
+  var instanceName =
+    settings.instanceName !== undefined ? settings.instanceName : "etherpad";
+
+  // Add custom views folder
+  const currentViews = args.app.get("views");
+  if (typeof currentViews == "object") {
+    args.app.set("views", [...currentViews, __dirname + "/views"]);
+  } else {
+    args.app.set("views", [currentViews, __dirname + "/views"]);
+  }
+
+  // Expose metrics
+  args.app.get("/metrics", function (req, res) {
+    res.render("metrics.ejs", {
+      instanceName: instanceName,
+      metrics: metrics,
+    });
+  });
+};
